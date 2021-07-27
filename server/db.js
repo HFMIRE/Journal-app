0ef43c3f3f037440bdb08cc51abd8d06aa41b3ac
HFMIRE <huda.f.mire@gmail.com>
2021-07-27 11:24:02 +0100

    setting up

---
+++ server/db.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const path = require("path");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+});
+class User extends Model {}
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "User" }
+);
+class Entries extends Model {}
+Entries.init(
+  {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+  },
+  { sequelize, modelName: "Entries" }
+);
+User.hasMany(Entries, { as: "entries", foreignKey: "UserId" });
+Entries.belongsTo(User, { foreignKey: "UserId" });
+sequelize.sync();
+module.exports = { sequelize, User };
