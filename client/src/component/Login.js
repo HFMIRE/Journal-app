622754e047e98d2dcf6c9d425a7fefd5a71a048e
HFMIRE <huda.f.mire@gmail.com>
2021-07-29 00:33:02 +0100

    routing to home not working

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,40 +1,40 @@
-import React, {useState} from 'react'
-import {Link} from "react-router-dom"
- 
-const Login = () => {
-    const [username, setUsername] = useState("")    
-    const [password, setPassword] = useState("")
-    return (
-        <div>
-            <h1>Login</h1>
-        <form >
-        <h1>Login</h1>
-          <label htmlFor="username">
-             Name:
-              <input 
-              id="username"
-              value = {username}
-              placeholder="Name"
-              onChange = {(e) => setUsername(e.target.value)}
-              required/>
-          </label>
-          <label htmlFor="password">
-             Password:
-              <input 
-              id="password"
-              value = {password}
-              placeholder="Password"
-              onChange = {(e) => setPassword(e.target.value)}
-              required/>
-          </label>
-          <Link to={"/Dashboard"}>
-          <button >
-              Register
-          </button>
-          </Link>
-            </form>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default Login
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="username">
+          Name:
+          <input
+            id="username"
+            value={username}
+            placeholder="Name"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            id="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <Link to={"/dashboard"}>
+          <button>Register</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
